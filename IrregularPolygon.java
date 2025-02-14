add3b8d78904b875ade42aed6e3ce14bd61fcab5
buddykins <throuse6@gmail.com>
2025-02-14 23:09:36 +0000

    everything minus .draw() works :)

--- IrregularPolygon.java
+++ IrregularPolygon.java
@@ -1,4 +1,5 @@
 import java.awt.geom.*; // for Point2D.Double
+import java.awt.geom.Point2D.Double;
 import java.util.ArrayList; // for ArrayList
 import java.util.concurrent.TimeUnit;
 
@@ -21,13 +22,58 @@ public class IrregularPolygon {
     }
 
     public double perimeter() {
-        // TODO: Calculate the perimeter.
-        return 3.14;
+        Point2D.Double current;
+        Point2D.Double next;
+        double dist = 0.0;
+        for(int j = 0; j < myPolygon.size(); j++){
+
+            if (j < myPolygon.size()-1){
+                current = myPolygon.get(j);
+                next = myPolygon.get(j+1);
+            }
+            else{
+                current = myPolygon.get(j);
+                next = myPolygon.get(0);
+            }
+           
+            dist += current.distance(next);
+            
+        } 
+           
+        return dist;
     }
 
     public double area() {
-        // TODO: Calculate the area.
-        return 0.0;
+        double res = 0.0;
+        double temp = 0.0;
+        Point2D.Double current;
+        Point2D.Double next;
+        for(int j = 0; j < myPolygon.size();j++){
+            if (j < myPolygon.size()-1){
+                current = myPolygon.get(j);
+                next = myPolygon.get(j+1);
+            }
+            else{
+                current = myPolygon.get(j);
+                next = myPolygon.get(0);
+            }
+            temp = current.getX() * next.getY();
+            res += temp;
+        }
+        for(int j = 0; j < myPolygon.size();j++){
+            if (j < myPolygon.size()-1){
+                current = myPolygon.get(j);
+                next = myPolygon.get(j+1);
+            }
+            else{
+                current = myPolygon.get(j);
+                next = myPolygon.get(0);
+            }
+            temp = current.getY() * next.getX();
+            res -= temp;
+        }
+        res *= 0.5;
+        return Math.abs(res);
     }
 
     public void draw()
